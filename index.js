fbce674d0752d5e940ce4d893f59f85a8808095d
milahu <milahu@gmail.com>
2021-03-06 15:57:08 +0100

    compile svelte submodules (proof of concept)

--- index.js
+++ index.js
@@ -108,7 +108,38 @@ module.exports = function (options = {}) {
 				code = processed.code;
 			}
 
+			// compile subcomponents
+			const sub_components = [];
+			const sub_component_names = new Set();
+			code = code.replace(/{#component(.*?)}(.*?){\/component}/sg, (sub_fullmatch, sub_attributes, sub_code, ...sub_restargs) => {
+				const sub_name = sub_attributes.trim();
+				if (sub_name.match(/[A-Z][A-Za-z0-9_]*/) == null) {
+					throw 'invalid subcomponent name: '+sub_name; // TODO is this raised? use this.warn()
+				}
+				const sub_filename = filename.replace(/(\.svelte)$/, '__' + sub_name + '$1');
+				const sub_svelte_options = { ...compilerOptions, filename: sub_filename };
+				let sub_compiled;
+				try {
+					sub_compiled = compile(sub_code, sub_svelte_options);
+					sub_components.push({ filename, sub_name, sub_filename, sub_code, sub_restargs, sub_compiled });
+					sub_component_names.add(sub_name);
+				} catch (compile_error) {
+					// TODO handle compile error
+					sub_components.push({ filename, sub_name, sub_filename, sub_code, sub_restargs, compile_error });
+				}
+				return ''; // TODO add empty newlines or fix sourcemaps, to keep line numbers
+			});
+
 			const compiled = compile(code, svelte_options);
+
+			// remove warnings
+			compiled.warnings = compiled.warnings.filter(w => {
+				if (w.code != 'missing-declaration') return true; // keep
+				// message: "'SubComponent' is not defined",
+				const decl_name = (w.message.match(/^'([^']+)'/) || [])[1];
+				if (sub_component_names.has(decl_name)) return false; // ignore this warning
+				return true;
+			});
 
 			(compiled.warnings || []).forEach(warning => {
 				if (!emitCss && warning.code === 'css-unused-selector') return;
@@ -126,6 +157,62 @@ module.exports = function (options = {}) {
 				dependencies.forEach(this.addWatchFile);
 			} else {
 				compiled.js.dependencies = dependencies;
+			}
+
+			// inject subcomponents into compiled.js.code
+			if (sub_components.length > 0) {
+				const parent_imports = new Set(
+					compiled.js.code
+						.match(/import {(.*?)} from "svelte\/internal";\n/s)[1]
+						.match(/[A-Za-z0-9_]+/g)
+				);
+
+				const parent_name = (
+					s = compiled.js.code,
+					a = s.lastIndexOf('export default ') + 'export default '.length,
+					s.slice(a, s.indexOf(';', a))
+				);
+
+				const inject_code = sub_components.map(sc => {
+					const js_code = sc.sub_compiled.js.code;
+					const cut_start = js_code.indexOf('"svelte/internal";') + '"svelte/internal";'.length;
+					const cut_end = js_code.lastIndexOf('export default ');
+					const sc_imports = js_code.slice(0, cut_start)
+						.match(/import {(.*?)} from "svelte\/internal";/s)[1]
+						.match(/[A-Za-z0-9_]+/g)
+						.filter(n => !parent_imports.has(n))
+					;
+					const import_code = (sc_imports.length == 0) ? '' : (
+						'import { ' + sc_imports.join(', ') + ' } from "svelte/internal";'
+					);
+					const ponent_code = js_code.slice(cut_start + 1, cut_end);
+					return [
+						import_code,
+						`${parent_name}.${sc.sub_name} = (() => {`,
+						// comments must be inserted here, otherwise not visible in bundle.js
+						`// subcomponent ${parent_name}.${sc.sub_name}`,
+						`${ponent_code}`,
+						`return ${parent_name}__${sc.sub_name};`,
+						`})();`,
+					].join('\n');
+				}).join('\n\n');
+
+				const idx_before_export = compiled.js.code.lastIndexOf('\nexport default ');
+
+				compiled.js.code = (
+					compiled.js.code.slice(0, idx_before_export) +
+					'\n\n// inject_code:\n\n' + inject_code + '\n\n// :inject_code\n\n' +
+					compiled.js.code.slice(idx_before_export + 1)
+				);
+
+				// fix subcomponent init calls
+				sub_components.forEach(sc => {
+					compiled.js.code = compiled.js.code.replace(
+						new RegExp(`( = new )(${sc.sub_name}\\({ )`, 'g'), // TODO avoid false matches (code vs data)
+						`$1${parent_name}.$2`
+					);
+				});
+
 			}
 
 			return compiled.js;
